527260fb036ddf76cc44ec3896f0fff7b18a57e1
hkprasadam <hemanthkumarprasadam48@gmail.com>
2024-12-20 10:02:05 +0530

    add js operators

---
+++ index.js
@@ -0,0 +1,29 @@
+//------And operator--------
+let x1=5;
+let x2=4;
+
+let result=!((x1>x2)&&(x1==x2));
+
+console.log(result)
+//--------or operator---------
+let obj1={
+       
+}
+
+
+let isNodejs=true;
+let isDJango=false;
+let exp=4;
+let joinin=10;
+let isrelocate=true;
+let isflexible=true;
+
+let job=((isNodejs||isDJango)&&(exp=4)&&(joinin<=15)&&(isrelocate&&isflexible))
+console.log(job)
+
+//-------conditional operators---------
+
+let username= "hemanth";
+let password= "hemu123";
+
+username == "hemanth" && password == "hemu123" ? console.log("login sucessfull"): console.log("login unsucessfull");
